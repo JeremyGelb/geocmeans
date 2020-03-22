328fb27d5dfa8813d8c8c77d51d6ac38e265648e
JeremyGelb <jeremy.gelb@ucs.inrs.ca>
2020-03-22 18:56:32 -0400

    Adding a small description of the full package and adding an example of how calcaulting the spatial consistency for the k-means in the vignette

--- R/geocmeans.R
+++ R/geocmeans.R
@@ -1,9 +1,24 @@
-#' geocmeans: A package implementing methods for spatially constrained c-ceans algorithm
+#' geocmeans: A package implementing methods for spatially constrained c-ceans
+#' algorithm
 #'
-#' The geocmeans, this section of the documentation is still to develop
+#' The geocmeans package implement a modified c-means algorithm more suited to
+#' work with spatial data (characterized by high spatial autocorrelation). The
+#' spatial information is introduced with a spatial weight #' matrix W (n * n)
+#' where wij indicate the strength of the spatial relationship between the
+#' obvservations i and j. Its is recommanded to use a matrix standardized by row
+#' (so that the sum of each row is 1). More specifically, the spatial c-means
+#' combine the euclidean distance of each observation in the data matrix X to
+#' each center with the euclidean distance of the lagged version of X by W (WX).
+#' A parameter alpha allow to control for the weight of the lagged matrice. If
+#' alpha = 0, then the spatial c-means is equal to a classical c-means. If alpha
+#' = 1, then the weights of the X and WX are equals. If alpha = 2, then the
+#' weight of WX is twice the one of X and so on.
 #'
-#' @section Foo functions:
-#' The foo functions ...
+#' A index to measure the spatial consistency of a classification is proposed in
+#' this package.
+#'
+#' More algorithm will be implemented in the future.
+#'
 #'
 #' @docType package
 #' @name geocmeans

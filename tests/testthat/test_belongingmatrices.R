a85b79b79a5062080c655d05f4936369af839e8a
JeremyGelb <jeremy.gelb@ucs.inrs.ca>
2021-04-17 00:07:54 -0400

    Adding the code of conduct and guidelines for contributions. Also creating a rmd version of the READEME

--- tests/testthat/test_belongingmatrices.R
+++ tests/testthat/test_belongingmatrices.R
@@ -1,95 +1,67 @@
 context("belonging matrices")
 
-test_that("SFCM belonging matrices must have a rowSums equal to 1",{
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### Testing that the obtained membership matrices have a rowSums = 1
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+test_that("Membership matrices must have a rowSums equal to 1",{
   data(LyonIris)
   library(spdep)
   AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
   dataset <- LyonIris@data[AnalysisFields]
   queen <- poly2nb(LyonIris,queen=TRUE)
   Wqueen <- nb2listw(queen,style="W")
-  result <- SFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 1.5, standardize = TRUE)
-  x <- round(1-rowSums(result$Belongings),6)
-  expect_equal(sum(x),0)
+  FCM <- CMeans(dataset, k = 5, m = 1.5, standardize = TRUE)
+  GFCM <- GCMeans(dataset, k = 5, m = 1.5, beta = 0.5, standardize = TRUE)
+  SFCM <- SFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 1.5, standardize = TRUE)
+  SGFCM <- SGFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 1.5, beta = 0.5, standardize = TRUE)
+  x1 <- round(1-rowSums(FCM$Belongings),6)
+  x2 <- round(1-rowSums(GFCM$Belongings),6)
+  x3 <- round(1-rowSums(SFCM$Belongings),6)
+  x4 <- round(1-rowSums(SGFCM$Belongings),6)
+  expect_equal(sum(x1) + sum(x2) + sum(x3) + sum(x4),0)
 })
 
-test_that("SFCM belonging matrices must have a rowSums equal to 1, with high alpha",{
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#### Testing that the algorithms have identical results if beta or alpha = 0
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+test_that("SFCM and FCM should yield identical results if alpha = 0 for SFCM",{
   data(LyonIris)
   library(spdep)
   AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
   dataset <- LyonIris@data[AnalysisFields]
   queen <- poly2nb(LyonIris,queen=TRUE)
   Wqueen <- nb2listw(queen,style="W")
-  result <- SFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 8, standardize = TRUE)
-  x <- round(1-rowSums(result$Belongings),6)
+  result1 <- SFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 0, standardize = TRUE, seed = 123)
+  result2 <- CMeans(dataset, k = 5, m = 1.5, standardize = TRUE, seed = 123)
+  x <- round(result1$Belongings - result2$Belongings,8)
   expect_equal(sum(x),0)
 })
 
-test_that("SFCM belonging matrices must have a rowSums equal to 1, with 0 alpha",{
+
+test_that("GFCM and FCM should yield identical results if beta = 0 for GFCM",{
+  data(LyonIris)
+  library(spdep)
+  AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
+  dataset <- LyonIris@data[AnalysisFields]
+  result1 <- GCMeans(dataset, k = 5, m = 1.5, beta = 0, standardize = TRUE, seed = 123)
+  result2 <- CMeans(dataset, k = 5, m = 1.5, standardize = TRUE, seed = 123)
+  x <- round(result1$Belongings - result2$Belongings,8)
+  expect_equal(sum(x),0)
+})
+
+
+test_that("SGFCM and FCM should yield identical results if beta = 0 and alpha = 0 for SGFCM",{
   data(LyonIris)
   library(spdep)
   AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
   dataset <- LyonIris@data[AnalysisFields]
   queen <- poly2nb(LyonIris,queen=TRUE)
   Wqueen <- nb2listw(queen,style="W")
-  result <- SFCMeans(dataset, Wqueen,k = 5, m = 1.5, alpha = 0, standardize = TRUE)
-  x <- round(1-rowSums(result$Belongings),6)
+  result1 <- SGFCMeans(dataset,Wqueen, k = 5, m = 1.5, alpha = 0, beta = 0, standardize = TRUE, seed = 123)
+  result2 <- CMeans(dataset, k = 5, m = 1.5, standardize = TRUE, seed = 123)
+  x <- round(result1$Belongings - result2$Belongings,8)
   expect_equal(sum(x),0)
-})
-
-
-test_that("When alpha is 0, the belonging matrix should be the same for the two methods",{
-  data(LyonIris)
-  library(spdep)
-  AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
-  data <- LyonIris@data[AnalysisFields]
-  queen <- poly2nb(LyonIris,queen=TRUE)
-  Wqueen <- nb2listw(queen,style="W")
-
-  for (i in 1:ncol(data)) {
-    data[, i] <- scale(data[, i])
-  }
-
-  wdata <- data
-  for (Name in names(data)) {
-    wdata[[Name]] <- spdep::lag.listw(Wqueen, data[[Name]])
-  }
-
-  centers <- data[sample(nrow(data), 4), ]
-
-  belongMat1 <- calcBelongMatrix(centers, data, 1.5)
-  belongMat2 <- calcSFCMBelongMatrix(centers, data, wdata, 1.5,0)
-
-  diff1 <- belongMat1 - belongMat2
-  tot1 <- round(sum(diff1),8)
-  expect_equal(tot1,0)
-})
-
-
-test_that("When alpha is 0, the centers should be the same for the two methods",{
-  data(LyonIris)
-  library(spdep)
-  AnalysisFields <-c("Lden","NO2","PM25","VegHautPrt","Pct0_14","Pct_65","Pct_Img","TxChom1564","Pct_brevet","NivVieMed")
-  data <- LyonIris@data[AnalysisFields]
-  queen <- poly2nb(LyonIris,queen=TRUE)
-  Wqueen <- nb2listw(queen,style="W")
-
-  for (i in 1:ncol(data)) {
-    data[, i] <- scale(data[, i])
-  }
-
-  wdata <- data
-  for (Name in names(data)) {
-    wdata[[Name]] <- spdep::lag.listw(Wqueen, data[[Name]])
-  }
-
-  centers <- data[sample(nrow(data), 4), ]
-
-  belongMat1 <- calcBelongMatrix(centers, data, 1.5)
-
-  centers1 <- as.matrix(calcCentroids(data,belongMat1,1.5))
-  centers2 <- as.matrix(calcSWFCCentroids(data, wdata, belongMat1, 1.5, 0))
-
-  diff1 <- centers1 - centers2
-  tot1 <- round(sum(diff1),8)
-  expect_equal(tot1,0)
 })
